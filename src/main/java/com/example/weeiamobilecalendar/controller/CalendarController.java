ccc9f9ccdd8f8057f71f996c3d0fc2066e23cf10
witaszek98@wp.pl <witaszek98@wp.pl>
2020-12-01 14:55:27 +0100

    serving a file

--- src/main/java/com/example/weeiamobilecalendar/controller/CalendarController.java
+++ src/main/java/com/example/weeiamobilecalendar/controller/CalendarController.java
@@ -6,6 +6,8 @@ import com.example.weeiamobilecalendar.service.WeeiaCalendarFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,7 +17,9 @@ public class CalendarController {
     CalndarService calndarService;
 
     @GetMapping("/calendar")
-    String getCalendar(@RequestParam("year") String year, @RequestParam ("month") String month) {
+    String getCalendar(@RequestParam("year") String year, @RequestParam ("month") String month, HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.setContentType("application/ics");
+        httpServletResponse.setHeader("Content-Disposition", "attachment; filename="+ year + "-" + month + ".ics");
         return calndarService.getCalendarEventsOfMonth(Long.parseLong(year),Long.parseLong(month));
     }
 }
